e1197735b1acf4f648112e5b50b22e5d2dbf6f56
rayyan-2007 <mrefxeditz@gmail.com>
2026-07-02 12:12:56 +0530

    count feature

--- React UI/src/App.jsx
+++ React UI/src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import Card from "./component/card/Card";
+import { useState } from "react";
+import Context from "./component/Context";
 const App = () => {
-    return (
-       <>
-    <div className="app-container">
-      <Card name="Joe Root" email="joe@gmail.com" btnname="click"/>
-      <Card name="Harry Brook" email="harry@gmail.com" btnname="click"/>
-      <Card name="Ben Stokes" email="ben@gmail.com" btnname="click"/>
-    </div>
-    </>
-    ) 
+
+   const [count, setCount] = useState(0);
+
+     return (
+     <> 
+      <Context.Provider value = {{  Count:count, SetCount:setCount }}>
+        <h1 className="count">Count:{count}</h1>
+       <div className="app-container">
+         <Card name="Joe Root" email="joe@gmail.com" btnname="click" />
+         <Card name="Harry Brook" email="harry@gmail.com" btnname="click" />
+         <Card name="Ben Stokes" email="ben@gmail.com" btnname="click" />
+       </div>
+      </Context.Provider>
+     </>
+    
+     )
+  
 }
 export default App
--- React UI/src/component/button/Button.jsx
+++ React UI/src/component/button/Button.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import Context from "../Context";
+
 const Button = ({ BtnName }) => {
+     
+     const data = useContext(Context);
+     console.log(data);
+    
     return (
      <>
-      <button className="card-button">{BtnName}</button>
-     </>
-    )
+      <button className="card-button" onClick={(()=>{data.SetCount(data.Count+1)})}>{BtnName}</button>
+    </>
+  )
 };
 export default Button;
